92db86e53a5cea4175c2aa356e8e36a29bba2b7e
Lorena <lorena@proesc.com>
2021-03-07 23:08:13 -0300

    Criado serviço para api

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+// import config from 'dotenv';
+
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3333/'
+    // baseURL: process.env.REACT_APP_API_URL
+})
+
+export default api;
